4a28547bbbcfb509916634058c82413be400e65a
Amit Kumar Gupta <theamit97@gmail.com>
2017-08-10 21:51:16 +0530

    Improving api functionality for the dsi app

--- models/collegeNotificationModel.js
+++ models/collegeNotificationModel.js
@@ -2,9 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CollegeNotificationSchema = new Schema({
+     messageTitle:{
+          type: String,
+          required: [true, "Message title cannot be left empty !"]
+     },
      message:{
           type: String,
           required: [true, "Message cannot be left empty !"]
+     },
+     messageLevel:{
+          type: String,
+          required: [true, "Message level cannot be left empty !"]
      }
 });
 
--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,12 @@ router.get('/', function(req, res) {
      res.send({Name: "Amit in routes"});
 });
 
+router.get('/collegeNotification', function(req, res) {
+          CollegeNotification.find({messageLevel: "college"}, function(err, docs) {
+          res.send(docs);     
+     });
+});
+
 //POST API IS STILL IN DEV MODE NOT FINAL !!!
 router.post('/collegeNotification', function(req, res) {
      CollegeNotification.create(req.body).then(function(collegeNotification) {
