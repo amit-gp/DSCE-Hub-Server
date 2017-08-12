82edf41d05f9d477ecd6101bbbc7bd9c6486b547
DESKTOP-MOKBNTP\Amit Kumar <theamit97@gmail.com>
2017-08-12 12:48:27 +0530

    Test

--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const CollegeNotification = require('../models/collegeNotificationModel');
 const router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/amit', function(req, res) {
      res.send({Name: "Amit in routes"});
 });
 
 router.get('/collegeNotification', function(req, res) {
           CollegeNotification.find({messageLevel: "college"}, function(err, docs) {
-          res.send(docs);     
+          res.send(docs);
      });
 });
 
